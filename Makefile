95ae2333221c95cd2302af0343520bd687983c7f
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-03 07:58:13 +0700

    save

---
+++ Makefile
@@ -0,0 +1,11 @@
+format:
+	clang-format -i *.c
+
+avggit:
+	git add *
+	git commit -m "save"
+	git push
+
+run:
+
+clear:
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void cicle(){
+
+}
+
+int main(int argc, char *argv[]){
+    
+    if(argc == 1){
+
+    }else{
+
+    }
+
+    return 0;
+}
